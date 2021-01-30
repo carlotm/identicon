5ad345d2d5ff909fda739aa42f265a50ea7b4469
Ascani Carlo <carlo.ratm@gmail.com>
2021-01-30 12:51:28 +0100

    Fix args fetching

--- lib/CLI.ex
+++ lib/CLI.ex
@@ -1,7 +1,7 @@
 defmodule Identicon.CLI do
   def main(args) do
-    {_, input, _} = OptionParser.parse(args, strict: [])
+    {_, [str | _], _} = OptionParser.parse(args, strict: [])
 
-    File.write(Identicon.filename(input), Identicon.from_string(input))
+    File.write(Identicon.filename(str), Identicon.from_string(str))
   end
 end
